6e4b4a186d49d6a78129619bdbc6c57e9a1727f9
czm <cezunigamunoz@gmail.com>
2021-12-15 11:58:25 -0500

    module order coordinator done

--- src/components/coordinator/orderCoordinator.jsx
+++ src/components/coordinator/orderCoordinator.jsx
@@ -1,15 +1,53 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Modal} from "react-bootstrap";
 import Constants, {icons} from "../../config/constants";
 import axios from "axios";
+import '../../styles/modal.css';
+import sweetAlert from "../../utils/SweetAlert";
 
 const OrderCoordinator = () => {
-    const [order, setOrder] = useState([]);
+    const [orders, setOrders] = useState([]);
+    const [order, setOrder] = useState(Constants.default_order);
     const [show, setShow] = useState(false);
+    const styleInputStatus = {
+        width: '70rem',
+        textAlign: 'center',
+        marginLeft: '12%'
+    }
 
-    const getOrder = async () => {
-        axios.get(`${Constants.url_order}/all`)
-            .then(result => setOrder(result.data))
+    useEffect(() => {
+        getOrder();
+    },[show])
+
+    const getOrder = () => {
+        const zone = JSON.parse(sessionStorage.getItem("user")).zone;
+        axios.get(`${Constants.url_order}/zona/${zone}`)
+            .then(result => setOrders(result.data))
+            .catch(error => console.log(error));
+    }
+
+    const showOrderDetails = async (orderId) => {
+        const orderDb = (await axios.get(`${Constants.url_order}/${orderId}`)).data;
+        setOrder(orderDb);
+        setShow(true);
+    }
+
+    const handleInputChange = (e) => {
+        setOrder({
+            ...order,
+            [e.currentTarget.id]: e.currentTarget.value
+        });
+    }
+
+    const saveStatusOrder = async () => {
+        const headers = Constants.headers;
+        const response = (await axios.put(`${Constants.url_order}/update`, order, {headers})).data;
+        if (response.id === null){
+            await sweetAlert.errorAlert("It was not possible to update order");
+            return;
+        }
+        await sweetAlert.successAlert("Order updated successfully");
+        setShow(false);
     }
 
     return (
@@ -35,20 +73,31 @@ const OrderCoordinator = () => {
                                 <th>Order Details</th>
                             </tr>
                             </thead>
-
                             <tbody className="table-content" id="tableContent">
+                            {
+                                orders.map((order, i) => (
+                                    <tr key={i}>
+                                        <td data-label="Identification">{order.salesMan.identification}</td>
+                                        <td data-label="Name">{order.salesMan.name}</td>
+                                        <td data-label="Email">{order.salesMan.email}</td>
+                                        <td data-label="Date">{order.registerDay.split("T")[0]}</td>
+                                        <td data-label="No. order">{order.id}</td>
+                                        <td data-label="Status">{order.status}</td>
+                                        <td data-label="Order Details"><span className="warning" onClick={() => showOrderDetails(order.id)}>{icons.details}</span></td>
+                                    </tr>
+                                ))
+                            }
                             </tbody>
-
                         </table>
                     </div>
                 </div>
             </section>
-            <Modal className="modal--active" show={show}>
+            <Modal className="modal--active" show={show} dialogClassName="modal-90w">
                 <Modal.Header>
-                    <Modal.Title>Product</Modal.Title>
+                    <Modal.Title>Order</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <table className="table">
+                    <table className="table" style={styleInputStatus}>
                         <thead>
                         <tr>
                             <th>Date</th>
@@ -58,18 +107,49 @@ const OrderCoordinator = () => {
                             <th>Save</th>
                         </tr>
                         </thead>
-
+                        <tbody className="table-content">
+                            <tr>
+                                <td data-label="Register day">{order.registerDay}</td>
+                                <td data-label="Order id">{order.id}</td>
+                                <td data-label="Status">{order.status}</td>
+                                <td data-label="Change status">
+                                    <select id="status" value={order.status} onChange={handleInputChange} className="form-control">
+                                        <option value="Pendiente">Pendiente</option>
+                                        <option value="Aprobado">Aprobado</option>
+                                        <option value="Rechazado">Rechazado</option>
+                                    </select>
+                                </td>
+                                <td data-label="Save"><span className="warning" onClick={() => saveStatusOrder()}>{icons.save}</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th>Photography</th>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Description</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Stock</th>
+                        </tr>
+                        </thead>
                         <tbody className="table-content" id="tablaProduct">
                         {
-                            // productsDB.map((product, i) => (
-                            //     <tr key={i}>
-                            //         <td data-label="Brand">{product.brand}</td>
-                            //         <td data-label="Category">{product.category}</td>
-                            //         <td data-label="Name">{product.name}</td>
-                            //         <td data-label="Price">{product.price}</td>
-                            //         <td data-label="Add"><span className="warning" onClick={() => addProduct(product)}>{icons.add}</span></td>
-                            //     </tr>
-                            // ))
+                            Object.values(order.products).map((product, i) => (
+                                <tr key={i}>
+                                    <td data-label="Photography">
+                                        <img src={product.photography} alt={product.category}/>
+                                    </td>
+                                    <td data-label="Name">{product.name}</td>
+                                    <td data-label="Category">{product.category}</td>
+                                    <td data-label="Description">{product.description}</td>
+                                    <td data-label="Price">{product.price}</td>
+                                    <td data-label="Quantity">{order.quantities[product.id]}</td>
+                                    <td data-label="Stock">{product.quantity}</td>
+                                </tr>
+                            ))
                         }
                         </tbody>
                     </table>
