bb50c6c574b85bc36679b6848bf359bb454abc37
czm <cezunigamunoz@gmail.com>
2021-12-14 21:35:55 -0500

    ase-order module done

--- src/components/sales-advisor/orderSalesAdvisor.jsx
+++ src/components/sales-advisor/orderSalesAdvisor.jsx
@@ -1,65 +1,88 @@
 import {useEffect, useState} from "react";
 import Constants, {icons} from "../../config/constants";
 import axios from "axios";
-import productOrder from "./Order";
 import {Modal} from "react-bootstrap";
+import sweetAlert from "../../utils/SweetAlert";
 
 const OrderSalesAdvisor =() => {
-    const [order, setOrder] = useState(Constants.default_order);
-    const [products, setProducts] = useState([]);
-    const [quantities, setQuantities] = useState({});
+    const [productsOrder, setProductsOrder] = useState([]);
+    const [productsDB, setProductsDB] = useState([]);
+    const [quantities, setQuantities] = useState([]);
     const [show, setShow] = useState(false);
-
-    document.addEventListener("DOMContentLoaded", async () => {
-        axios.get(`${Constants.url_gadget}/all`)
-            .then(result => setProducts(result.data))
-            .catch(error => console.log(error));
-    });
 
     useEffect(() => {
     }, []);
 
-     const handleInputChangeQuantity = (e) => {
-         setQuantities({
-             ...quantities,
-             [e.currentTarget.id]: e.currentTarget.value
-         });
-     }
+    const loadProducts = () => {
+        axios.get(`${Constants.url_gadget}/all`)
+            .then(result => setProductsDB(result.data))
+            .catch(error => console.log(error))
+    }
+
+    const showProducts = () => {
+        loadProducts();
+        setShow(true);
+    }
 
     const handleInputChange = (e) => {
-        setOrder({
-            ...order,
+        setQuantities({
+            ...quantities,
             [e.currentTarget.id]: e.currentTarget.value
         });
+        console.log(quantities);
     }
 
-    const filterProducts = () => {
-        const prodInOrder = productOrder.getProducts();
-        const productsFilter = [];
-        products.forEach(product => {
-            prodInOrder.forEach(p => {
-                if (product.id !== p.id) productsFilter.push(product);
-            })
-        });
-        return productsFilter;
+    /**
+     * Todo: Filter product already in productOrder
+     * Todo: Change for
+     * @param prod
+     */
+    const addProduct = (prod) => {
+        productsOrder.push(prod);
+        setProductsDB(productsDB);
+        setShow(false);
     }
 
-    const loadProducts = () => {
-         if (products.length > 0){
-             const productsTemp = filterProducts().length > 0 ?
-                 filterProducts() :
-                 productOrder.getProducts().length !== products.length ? products : []
-             setProducts(productsTemp);
-         }
-         setShow(true);
+    const deleteProduct = (id) => {
+        setProductsOrder(productsOrder.filter(p => p.id !== id));
     }
 
-    const addProduct = (prodId) => {
-         productOrder.addProduct(products.filter(p => p.id === prodId)[0]);
+    const getCurrentDate = () => {
+        const offset = new Date().getTimezoneOffset();
+        const date = new Date(new Date().getTime() - offset * 60 * 1000);
+        return date.toISOString().split("T")[0];
     }
 
-    const orderProducts = () => {
-         setProducts(productOrder.getProducts);
+    const sendOrder = async () => {
+        if (productsOrder.length === 0){
+            await sweetAlert.errorAlert('You must add at least one product');
+            return;
+        }
+        if (Object.keys(quantities).length === 0 || Object.keys(quantities).length !== productsOrder.length){
+            await sweetAlert.errorAlert("You must add quantity in the corresponding field");
+            return;
+        }
+        const salesMan = await axios(`${Constants.url_user}/${sessionStorage.getItem("user")}`);
+        const productsToSend = {}
+        for (let i = 0; i<productsOrder.length; i++){
+            productsToSend[productsOrder[i].id] = productsOrder[i]
+        }
+        const data = {
+            registerDay: getCurrentDate(),
+            status: "Pendiente",
+            salesMan: salesMan.data,
+            products: productsToSend,
+            quantities: {...quantities}
+        };
+        const headers = Constants.headers;
+        const order = await axios.post(`${Constants.url_order}/new`, JSON.stringify(data), {headers});
+        if (order?.id === null){
+            await sweetAlert.errorAlert("It was not possible to create order");
+            return;
+        }
+        await sweetAlert.successAlert(`Order ${order.id} added successfully`);
+        setProductsOrder([]);
+        setQuantities([]);
     }
 
     return (
@@ -71,8 +94,8 @@ const OrderSalesAdvisor =() => {
                 <h1>Order - ASE</h1>
                 <div className="content">
                     <div className="btn__container">
-                        <button type="button" onClick={() => loadProducts()}>Add Product</button>
-                        <button type="button" id="btnSendOrder">Send Order</button>
+                        <button type="button" onClick={() => showProducts()}>Add Product</button>
+                        <button type="button" onClick={() => sendOrder()}>Send Order</button>
                     </div>
                     <div className="table__container">
                         <table className="table">
@@ -87,14 +110,36 @@ const OrderSalesAdvisor =() => {
                                 <th>Delete</th>
                             </tr>
                             </thead>
-                            <tbody className="table-content" id="tableContent"/>
+                            <tbody className="table-content" id="tableContent">
+                            {
+                                productsOrder.map((product, i) => (
+                                    <tr key={i}>
+                                        <td data-label="name">{product.name}</td>
+                                        <td data-label="Category">{product.category}</td>
+                                        <td data-label="Description">{product.description}</td>
+                                        <td data-label="Price">{product.price}</td>
+                                        <td data-label="Photography">
+                                            <img src={product.photography} alt={product.name}/>
+                                        </td>
+                                        <td data-label="Quantity">
+                                            <input id={product.id}
+                                                   type="number"
+                                                   placeholder="0"
+                                                   onChange={handleInputChange}
+                                            />
+                                        </td>
+                                        <td data-label="Add"><span className="warning" onClick={() => deleteProduct(product.id)}>{icons.delete}</span>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                            </tbody>
                         </table>
 
                     </div>
                 </div>
             </section>
             <Modal className="modal--active" show={show}>
-                <Modal.Dialog>
                     <Modal.Header>
                         <Modal.Title>Product</Modal.Title>
                     </Modal.Header>
@@ -112,22 +157,13 @@ const OrderSalesAdvisor =() => {
 
                             <tbody className="table-content" id="tablaProduct">
                             {
-                                products.map((product, i) => (
+                                productsDB.map((product, i) => (
                                     <tr key={i}>
+                                        <td data-label="Brand">{product.brand}</td>
+                                        <td data-label="Category">{product.category}</td>
                                         <td data-label="Name">{product.name}</td>
-                                        <td data-label="Category">${product.category}</td>
-                                        <td data-label="Description">${product.name}</td>
-                                        <td data-label="Price">${product.price}</td>
-                                        <td data-label="Photography">
-                                            <img src={product.photography} alt={product.name}/>
-                                        </td>
-                                        <td data-label="Quantity">
-                                            <input type="number"
-                                                   placeholder="0"
-                                                   onChange={handleInputChangeQuantity}
-                                                   value={productOrder.getQuantity(product.id) || 0}/>
-                                        </td>
-                                        <td><span onClick={() => addProduct(product.id)}>{icons.add}</span></td>
+                                        <td data-label="Price">{product.price}</td>
+                                        <td data-label="Add"><span className="warning" onClick={() => addProduct(product)}>{icons.add}</span></td>
                                     </tr>
                                 ))
                             }
@@ -137,7 +173,6 @@ const OrderSalesAdvisor =() => {
                     <Modal.Footer>
                         <button className="btn btn-secondary" onClick={() => setShow(false)}>Cancel</button>
                     </Modal.Footer>
-                </Modal.Dialog>
             </Modal>
         </main>
     )
